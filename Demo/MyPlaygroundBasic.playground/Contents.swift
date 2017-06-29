b2c3f237d5ca175a2afde190e810e83fc281829b
Clay Zhu <clayzhu@163.com>
2017-06-29 16:57:46 +0800

    下标脚本。

--- Demo/MyPlaygroundBasic.playground/Contents.swift
+++ Demo/MyPlaygroundBasic.playground/Contents.swift
@@ -494,3 +494,34 @@ struct SubscriptStruct {
 let division = SubscriptStruct(decrementer: 100)
 print("100 / 9 = \(division[9])")
 print("100 / 2 = \(division[2])")
+// 下标脚本选项
+struct MatrixStruct {
+    let rows: Int, columns: Int
+    var print: [Double]
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        print = Array(repeating: 0.0, count: rows * columns)
+    }
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            return print[(row * columns) + column]
+        }
+        set {
+            print[(row * columns) + column] = newValue
+        }
+    }
+}
+// 创建了一个新的 3 行 3 列的 MatrixStruct 实例
+var mat = MatrixStruct(rows: 3, columns: 3)
+// 通过下标脚本设置值
+mat[0,0] = 1.0
+mat[0,1] = 2.0
+mat[1,0] = 3.0
+mat[1,1] = 5.0
+// 通过下标脚本获取值
+print("\(mat)")
+print("\(mat[0,0])")
+print("\(mat[0,1])")
+print("\(mat[1,0])")
+print("\(mat[1,1])")
