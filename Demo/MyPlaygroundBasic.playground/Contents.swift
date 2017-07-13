8af701331aa78a488ad628eea385f4a246d5d917
Clay Zhu <clayzhu@163.com>
2017-07-14 00:58:28 +0800

    可选链。

--- Demo/MyPlaygroundBasic.playground/Contents.swift
+++ Demo/MyPlaygroundBasic.playground/Contents.swift
@@ -673,11 +673,21 @@ print(counter)
 
 // 可选链
 class Person {
-    
+    var residence: Residence?
 }
 // 定义了一个变量 rooms，它被初始化为一个Room[]类型的空数组
 class Residence {
-    
+    var rooms = [Room]()
+    var numberOfRooms: Int {
+        return rooms.count
+    }
+    subscript(i: Int) -> Room {
+        return rooms[i]
+    }
+    func printNumberOfRooms() {
+        print("房间号为 \(numberOfRooms)")
+    }
+    var address: Address?
 }
 // Room 定义一个name属性和一个设定room名的初始化器
 class Room {
@@ -700,4 +710,10 @@ class Address {
             return nil
         }
     }
+}
+let john = Person()
+if john.residence?.printNumberOfRooms() != nil {
+    print("输出房间号")
+} else {
+    print("无法输出房间号")
 }
