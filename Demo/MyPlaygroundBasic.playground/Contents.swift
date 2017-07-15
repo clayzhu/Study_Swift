b1e404027336dfa7c46922d64dd780a668a72665
Clay Zhu <clayzhu@163.com>
2017-07-16 00:45:46 +0800

    扩展

--- Demo/MyPlaygroundBasic.playground/Contents.swift
+++ Demo/MyPlaygroundBasic.playground/Contents.swift
@@ -859,3 +859,34 @@ for item in saprint {
         print("数学主题是: '\(example.physics)',  \(example.formulae)")
     }
 }
+
+// 扩展
+// 计算型属性
+extension Int {
+    var add: Int {
+        return self + 100
+    }
+    var sub: Int {
+        return self - 10
+    }
+    var mul: Int {
+        return self * 10
+    }
+    var div: Int {
+        return self / 5
+    }
+}
+let addition2 = 3.add
+print("加法运算后的值：\(addition2)")
+
+let subtraction = 120.sub
+print("减法运算后的值：\(subtraction)")
+
+let multiplication = 39.mul
+print("乘法运算后的值：\(multiplication)")
+
+let division2 = 55.div
+print("除法运算后的值: \(division2)")
+
+let mix = 30.add + 34.sub
+print("混合运算结果：\(mix)")
