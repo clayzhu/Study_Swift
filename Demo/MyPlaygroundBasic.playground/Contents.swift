7e1c792f87178e43952d38dec3469255dc155532
Clay Zhu <clayzhu@163.com>
2017-08-06 01:59:17 +0800

    访问控制。

--- Demo/MyPlaygroundBasic.playground/Contents.swift
+++ Demo/MyPlaygroundBasic.playground/Contents.swift
@@ -977,3 +977,9 @@ extension Stack {
 if let topItem = stackOfStrings.topItem {
     print("栈中的顶部元素是：\(topItem).")
 }
+
+// 访问控制
+// public 可以访问自己模块中源文件里的任何实体，别人也可以通过引入该模块来访问源文件里的所有实体。
+// internal 可以访问自己模块中源文件里的任何实体，但是别人不能访问该模块中源文件里的实体。
+// fileprivate 文件内私有，只能在当前源文件中使用。
+// private 只能在类中访问，离开了这个类或者结构体的作用域外面就无法访问。
