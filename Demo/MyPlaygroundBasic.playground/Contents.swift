4ed796ee25a1cd4864877afd7c24eb3de81f4f44
Clay Zhu <clayzhu@163.com>
2017-08-05 13:59:05 +0800

    协议类型.

--- Demo/MyPlaygroundBasic.playground/Contents.swift
+++ Demo/MyPlaygroundBasic.playground/Contents.swift
@@ -929,3 +929,18 @@ print(studdet.result)
 print(studdet.present)
 print(studdet.subject)
 print(studdet.stname)
+
+// 协议类型
+protocol Generator {
+    associatedtype members
+    func next() -> members?
+}
+var items = [10, 20, 30].makeIterator()
+while let x = items.next() {
+    print(x)
+}
+for lists in [1, 2, 3].map({i in i * 5}) {
+    print(lists)
+}
+print([100, 200, 300])
+print([1, 2, 3].map({i in i * 10}))
