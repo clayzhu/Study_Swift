873044fe9ba2239e7f29adcccdf9671d2927ef81
Clay Zhu <clayzhu@163.com>
2021-07-20 01:06:49 +0800

    learn Basic Operators

--- Demo/LanguageGuideDemo.playground/Pages/1. The Basics.xcplaygroundpage/Contents.swift
+++ Demo/LanguageGuideDemo.playground/Pages/1. The Basics.xcplaygroundpage/Contents.swift
@@ -2,24 +2,24 @@
 
 import Foundation
 
-// 数值型字面量
+// # 数值型字面量
 let decimalDouble = 12.1875
 let exponentDouble = 1.21875e1
 let hexadecimalDouble = 0xC.3p0
 
-// 类型别名
+// # 类型别名
 typealias AudioSample = UInt16
 var maxAmplitudeFound = AudioSample.min
 // maxAmplitudeFound 现在是 0
 var maxAudioSample = AudioSample.max
 
-// 元组
+// # 元组
 let http404Error = (404, "Not Found")
 let (statusCode, statusMessage) = http404Error
 let (justTheStatusCode, _) = http404Error
 let http200Status = (statusCode: 200, description: "OK")
 
-// 可选类型
+// # 可选类型
 var serverResponseCode: Int? = 404
 if serverResponseCode != nil {
     print("serverResponseCode: \(serverResponseCode!)")
@@ -39,7 +39,7 @@ let forcedString: String = possibleString! // 需要感叹号来获取值
 let assumedString: String! = "An implicitly unwrapped optional string." // 隐式解析可选类型
 let implicitString: String = assumedString  // 不需要感叹号
 
-// 断言和先决条件
+// # 断言和先决条件
 //let age = -3
 let age = 3
 assert(age >= 0, "A person's age cannot be less than zero")
---
+++ Demo/LanguageGuideDemo.playground/Pages/2. Basic Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// # 算术运算符
+// ## 求余运算符
+let remainder = 9 % -4
+let remainder2 = 9 % 4
+
+//: [Next](@next)
