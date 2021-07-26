fa56f0b74793bd9cc774f2ecc88165566d12bfe6
Clay Zhu <clayzhu@163.com>
2021-07-26 23:47:11 +0800

    markup

--- Demo/LanguageGuideDemo.playground/Pages/2. Basic Operators.xcplaygroundpage/Contents.swift
+++ Demo/LanguageGuideDemo.playground/Pages/2. Basic Operators.xcplaygroundpage/Contents.swift
@@ -2,16 +2,16 @@
 
 import Foundation
 
-// # 算术运算符
-// ## 求余运算符
+//: # 算术运算符
+//: ## 求余运算符
 let remainder = 9 % -4
 let remainder2 = 9 % 4
 
-// # 比较运算符
+//: # 比较运算符
 (1, 2, 3, 4, 5, 6) < (1, 2, 3, 4, 5, 6)
 //(1, 2, 3, 4, 5, 6, 7) == (1, 2, 3, 4, 5, 6, 7)    // Swift 标准库只能比较七个以内元素的元组比较函数。如果你的元组元素超过七个时，你需要自己实现比较运算符。
 
-// # 空合运算符
+//: # 空合运算符
 let defaultColor = "red"
 var userColor: String?
 var colorToUse = userColor ?? defaultColor
@@ -19,7 +19,7 @@ var colorToUse = userColor ?? defaultColor
 userColor = "green"
 colorToUse = userColor ?? defaultColor
 
-// # 区间运算符
+//: # 区间运算符
 let names = ["Anna", "Alex", "Brian", "Jack"]
 // ## 单侧区间
 for name in names[2...] {
--- Demo/LanguageGuideDemo.playground/Pages/3. Strings and Characters.xcplaygroundpage/Contents.swift
+++ Demo/LanguageGuideDemo.playground/Pages/3. Strings and Characters.xcplaygroundpage/Contents.swift
@@ -2,8 +2,8 @@
 
 import Foundation
 
-// # 字符串字面量
-// ## 多行字符串字面量
+//: # 字符串字面量
+//: ## 多行字符串字面量
 let quotation = """
 I am
 Clay
@@ -21,22 +21,22 @@ let multiLine2 = """
     """
 let isSame2 = multiLine == multiLine2
 
-// ## 扩展字符串分隔符
+//: ## 扩展字符串分隔符
 let delimiterStr = #"some\nstring"#
 let delimiterStr2 = #"some\#nstring"#
 print(delimiterStr)
 print(delimiterStr2)
 let isSame3 = delimiterStr == delimiterStr2
 
-// # 连接字符串和字符
+//: # 连接字符串和字符
 let exclamationMark: Character = "!"
 var welcome = "hello" + " "
 welcome.append("there")
 welcome.append(exclamationMark)
 
-// # Unicode
-// 建议阅读：https://juejin.cn/post/6844903543279730702
-// ## 可扩展的字形群集
+//: # Unicode
+//: > 建议阅读：[https://juejin.cn/post/6844903543279730702](https://juejin.cn/post/6844903543279730702)
+//: ## 可扩展的字形群集
 let eAcute: Character = "\u{E9}"
 let combinedEAcute: Character = "\u{65}\u{301}"
 
@@ -45,5 +45,7 @@ let enclosedEAcute: Character = "\u{E9}\u{20DD}"
 let regionalIndicatorForU: Character = "\u{1F1FA}"
 let regionalIndicatorForS: Character = "\u{1F1F8}"
 let regionalIndicatorForUS: Character = "\u{1F1FA}\u{1F1F8}"
+
+//: # 访问和修改字符串
 
 //: [Next](@next)
